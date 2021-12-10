b0a0b261985bebb686c8599b9ff7926194c804d2
Khoa Nguyen <khoadnguyen.se@gmail.com>
2021-12-11 01:43:00 +0700

    disable line number on treemacs buffer

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -78,6 +78,7 @@
   :init
   (with-eval-after-load 'winum
     (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :hook ((treemacs-mode . (lambda() (display-line-numbers-mode -1))))
   :config
   (progn
     (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
