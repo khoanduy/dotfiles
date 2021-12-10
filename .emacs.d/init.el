0db428027b4040f93b7a86036a9604a4b0c1fc77
Khoa Nguyen <khoadnguyen.se@gmail.com>
2021-12-11 01:35:14 +0700

    treemacs and cpp conf for emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,4 @@
-;;; init.el -- My Emacs configuration
-                                        ;-*-Emacs-Lisp-*-
+;;; init.el -- My Emacs configuration -*- lexical-binding: t; -*-
 
 ;;; Commentary:
 ;;
@@ -15,8 +14,6 @@
        ";; Khoa Nguyen\n"))
 
 (package-initialize)
-
-(add-to-list 'load-path (expand-file-name "lispc" user-emacs-directory))
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file 'noerror)
@@ -75,6 +72,97 @@
   :config
   (which-key-mode t))
 
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay        0.5
+          treemacs-directory-name-transformer      #'identity
+          treemacs-display-in-side-window          t
+          treemacs-eldoc-display                   t
+          treemacs-file-event-delay                5000
+          treemacs-file-extension-regex            treemacs-last-period-regex-value
+          treemacs-file-follow-delay               0.2
+          treemacs-file-name-transformer           #'identity
+          treemacs-follow-after-init               t
+          treemacs-expand-after-init               t
+          treemacs-git-command-pipe                ""
+          treemacs-goto-tag-strategy               'refetch-index
+          treemacs-indentation                     2
+          treemacs-indentation-string              " "
+          treemacs-is-never-other-window           nil
+          treemacs-max-git-entries                 5000
+          treemacs-missing-project-action          'ask
+          treemacs-move-forward-on-expand          nil
+          treemacs-no-png-images                   nil
+          treemacs-no-delete-other-windows         t
+          treemacs-project-follow-cleanup          nil
+          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                        'left
+          treemacs-read-string-input               'from-child-frame
+          treemacs-recenter-distance               0.1
+          treemacs-recenter-after-file-follow      nil
+          treemacs-recenter-after-tag-follow       nil
+          treemacs-recenter-after-project-jump     'always
+          treemacs-recenter-after-project-expand   'on-distance
+          treemacs-litter-directories              '("/target" "/build" "/node_modules" "/.venv" "/.cask")
+          treemacs-show-cursor                     nil
+          treemacs-show-hidden-files               t
+          treemacs-silent-filewatch                nil
+          treemacs-silent-refresh                  nil
+          treemacs-sorting                         'alphabetic-asc
+          treemacs-select-when-already-in-treemacs 'move-back
+          treemacs-space-between-root-nodes        t
+          treemacs-tag-follow-cleanup              t
+          treemacs-tag-follow-delay                1.5
+          treemacs-text-scale                      nil
+          treemacs-user-mode-line-format           nil
+          treemacs-user-header-line-format         nil
+          treemacs-wide-toggle-width               70
+          treemacs-width                           35
+          treemacs-width-increment                 1
+          treemacs-width-is-initially-locked       t
+          treemacs-workspace-switch-cleanup        nil)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode 'always)
+    (treemacs-resize-icons 13)
+
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple)))
+
+    (treemacs-hide-gitignored-files-mode nil))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :hook (dired-mode . treemacs-icons-dired-enable-once)
+  :ensure t)
+
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :ensure t)
+
 (use-package magit
   :ensure t
   :defer t)
@@ -100,10 +188,13 @@
   (setq helm-ff-search-library-in-sexp t)
   (setq helm-echo-input-in-header-line t)
   (setq helm-scroll-amount 9)
-  (setq helm-autoresize-max-height 0)
-  (setq helm-autoresize-min-height 20)
+  (setq helm-autoresize-max-height 20)
+  (setq helm-autoresize-min-height 10)
   (setq helm-autoresize-mode t)
   (helm-mode 1))
+
+(use-package helm-xref
+  :ensure t)
 
 (use-package projectile
   :ensure t
@@ -127,7 +218,8 @@
 (use-package company
   :ensure t
   :config
-  (setq company-idle-delay 0.5)
+  (setq company-idle-delay 0.1)
+  (setq company-minimum-prefix-length 0.1)
   (setq company-selection-wrap-around t)
   (define-key company-active-map (kbd "ESC") 'company-abort)
   (define-key company-active-map [tab] 'company-complete-common-or-cycle)
@@ -139,25 +231,39 @@
   :ensure t
   :init
   (setq lsp-keymap-prefix "C-c l")
-  :hook ((c++-mode . lsp-deferred)
+  :hook ((c-mode . lsp-deferred)
+         (c++-mode . lsp-deferred)
          (java-mode . lsp-deferred)
          (lsp-mode . lsp-enable-which-key-integration))
-  :commands (lsp lsp-deferred))
+  :commands (lsp lsp-deferred)
+  :config
+  (setq lsp-idle-delay 0.1)
+  (setq lsp-modeline-diagnostics-enable t)
+  (setq lsp-signature-auto-activate t)
+  (setq lsp-signature-render-documentation t))
 
 (use-package lsp-ui
   :ensure t
   :defer t
-  :commands lsp-ui-mode)
+  :commands lsp-ui-mode
+  :custom
+  lsp-ui-sideline-enable nil)
 
 (use-package helm-lsp
   :ensure t
   :defer t
   :commands helm-lsp-workspace-symbol)
 
+(use-package lsp-treemacs
+  :ensure t
+  :defer t
+  :config (lsp-treemacs-sync-mode 1))
+
 (use-package dap-mode
   :ensure t
   :defer t
   :config
+  (require 'dap-cpptools)
   (dap-mode 1)
   (dap-ui-mode 1)
   (dap-tooltip-mode 1)
