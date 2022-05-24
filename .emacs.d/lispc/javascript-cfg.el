22f2be0fbbf0ebd7faaf5cdffe78d5b60da4a2c8
khoarx <khoadnguyen.se@gmail.com>
2022-05-24 16:00:20 +0700

    tsx config

--- .emacs.d/lispc/javascript-cfg.el
+++ .emacs.d/lispc/javascript-cfg.el
@@ -2,11 +2,26 @@
 ;;; Commentary:
 ;;; Code:
 
+(use-package web-mode
+  :mode
+  ("\\.ejs\\'" "\\.hbs\\'" "\\.html\\'" "\\.[jt]sx?\\'")
+  :config
+  (setq web-mode-content-types-alist '(("jsx" . "\\.[jt]sx?\\'")))
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-script-padding 2)
+  (setq web-mode-block-padding 2)
+  (setq web-mode-style-padding 2)
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-auto-closing t)
+  (setq web-mode-enable-current-element-highlight t))
+
 (use-package js2-mode
   :mode
-  (("\\.js\\'" . js2-mode)
-   ("\\.jsx\\'" . js2-jsx-mode))
-  :hook ((js2-mode . lsp))
+  (("\\.js\\'" . js2-mode))
+  :hook ((js2-mode . lsp)
+	 (js2-mode . webmode))
   :bind (:map js2-mode-map
 	 ("M-." . lsp-find-definition))
   :config
@@ -25,10 +40,11 @@
          ("C-M \\" . json-mode-beautify)))
 
 (use-package typescript-mode
-  :hook ((typescript-mode . lsp))
+  :hook ((typescript-mode . lsp)
+	 (typescript-mode . web-mode))
   :bind (("M-." . lsp-find-definition)))
 
-(use-package prettier-js)
+(use-package add-node-modules-path)
 
 (provide 'javascript-cfg)
 ;;; javascript-cfg.el ends here
