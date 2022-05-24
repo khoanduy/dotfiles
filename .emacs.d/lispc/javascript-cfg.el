fc115d15ed5bee469e20cd8fb1ecf5fcee01feec
khoarx <khoadnguyen.se@gmail.com>
2022-05-24 12:16:53 +0700

    remove custom.el

--- .emacs.d/lispc/javascript-cfg.el
+++ .emacs.d/lispc/javascript-cfg.el
@@ -4,7 +4,8 @@
 
 (use-package js2-mode
   :mode
-  (("\\.js\\'" . js2-mode))
+  (("\\.js\\'" . js2-mode)
+   ("\\.jsx\\'" . js2-jsx-mode))
   :custom
   (js2-include-node-externs t)
   (js2-highlight-level 3)
@@ -15,11 +16,10 @@
   :hook ((js2-mode . company-mode)
          (js2-mode . flycheck-mode))
   :config
-  (with-eval-after-load 'js
-    (sanityinc/major-mode-lighter 'js-mode "JS")
-    (sanityinc/major-mode-lighter 'js-jsx-mode "JSX"))
   (setq js-indent-level 2)
   (setq js2-basic-offset 2))
+
+(add-to-list 'interpreter-mode-alist (cons "node" 'js2-mode))
 
 (use-package json-mode
   :custom
@@ -31,6 +31,10 @@
   :bind (:package json-mode-map
          :map json-mode-map
          ("C-M \\" . json-mode-beautify)))
+
+(use-package typescript-mode)
+
+(use-package prettier-js)
 
 (provide 'javascript-cfg)
 ;;; javascript-cfg.el ends here
