0ba18ae7a7b953336765e6252a2c44763a9c0d08
Khoa Nguyen <khoadnguyen.se@gmail.com>
2022-06-30 15:08:52 +0700

    toggle term config

--- .config/nvim/lua/mapping.lua
+++ .config/nvim/lua/mapping.lua
@@ -5,57 +5,70 @@ local api = vim.api
 -- Set leader key
 g.mapleader = ','
 
+-- Mapping options
+local nenrs = { expr = false, noremap = true, silent = true }
+local nenrns = { expr = false, noremap = true, silent = false }
+
 -- Remap C-w prefix
-api.nvim_set_keymap('n', '<leader>w', '<C-w>', {expr = false, noremap = true})
+api.nvim_set_keymap('n', '<leader>w', '<C-w>', nenrs)
 
 -- Remap switch region keys
-api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {expr = false, noremap = true, silent = true})
-api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {expr = false, noremap = true, silent = true})
-api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {expr = false, noremap = true, silent = true})
-api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {expr = false, noremap = true, silent = true})
+api.nvim_set_keymap('n', '<C-h>', '<C-w>h', nenrs)
+api.nvim_set_keymap('n', '<C-j>', '<C-w>j', nenrs)
+api.nvim_set_keymap('n', '<C-k>', '<C-w>k', nenrs)
+api.nvim_set_keymap('n', '<C-l>', '<C-w>l', nenrs)
 
 -- Re-size split windows using arrow keys
-api.nvim_set_keymap('n', '<Up>', ':resize -2<CR>', {expr = false, noremap = true})
-api.nvim_set_keymap('n', '<Down>', ':resize +2<CR>', {expr = false, noremap = true})
-api.nvim_set_keymap('n', '<Left>', ':vertical resize +2<CR>', {expr = false, noremap = true})
-api.nvim_set_keymap('n', '<Right>', ':vertical resize -2<CR>', {expr = false, noremap = true})
+api.nvim_set_keymap('n', '<Up>', ':resize -2<CR>', nenrns)
+api.nvim_set_keymap('n', '<Down>', ':resize +2<CR>', nenrns)
+api.nvim_set_keymap('n', '<Left>', ':vertical resize +2<CR>', nenrns)
+api.nvim_set_keymap('n', '<Right>', ':vertical resize -2<CR>', nenrns)
 
 -- Remap switch buffers keys
-api.nvim_set_keymap('n', '<leader>bn', ':bn<CR>', {expr = false, noremap = false})
-api.nvim_set_keymap('n', '<leader>bp', ':bp<CR>', {expr = false, noremap = false})
-api.nvim_set_keymap('n', '<leader>bd', ':bd<CR>', {expr = false, noremap = false})
-api.nvim_set_keymap('n', '<leader>bw', ':bw<CR>', {expr = false, noremap = false})
+api.nvim_set_keymap('n', '<leader>bn', ':bn<CR>', nenrs)
+api.nvim_set_keymap('n', '<leader>bp', ':bp<CR>', nenrs)
+api.nvim_set_keymap('n', '<leader>bd', ':bd<CR>', nenrs)
+api.nvim_set_keymap('n', '<leader>bw', ':bw<CR>', nenrs)
+api.nvim_set_keymap('n', '<leader>bW', ':bw!<CR>', nenrs)
 
 -- Remap terminal escape key
-api.nvim_set_keymap('t', '<leader>e', [[<C-\><C-n>]], {expr = false, noremap = true})
+api.nvim_set_keymap('t', '<leader>e', [[<C-\><C-n>]], nenrs)
 
 -- Search current marked text
-api.nvim_set_keymap('v', '//', [[y/\V<C-R>=escape(@",'/\')<CR><CR>]], {expr = false, noremap = true})
+api.nvim_set_keymap('v', '//', [[y/\V<C-R>=escape(@",'/\')<CR><CR>]], nenrs)
 
 -- NVIM tree
-api.nvim_set_keymap('n', '<leader>nt', ':NvimTreeToggle<CR>', {expr = false, noremap = true})
-api.nvim_set_keymap('n', '<leader>nf', ':NvimTreeFocus<CR>', {expr = false, noremap = true})
-api.nvim_set_keymap('n', '<leader>nF', ':NvimTreeFindFile<CR>', {expr = false, noremap = true})
-api.nvim_set_keymap('n', '<leader>nc', ':NvimTreeCollapse<CR>', {expr = false, noremap = true})
+api.nvim_set_keymap('n', '<leader>nt', ':NvimTreeToggle<CR>', nenrs)
+api.nvim_set_keymap('n', '<leader>nf', ':NvimTreeFocus<CR>', nenrs)
+api.nvim_set_keymap('n', '<leader>nF', ':NvimTreeFindFile<CR>', nenrs)
+api.nvim_set_keymap('n', '<leader>nc', ':NvimTreeCollapse<CR>', nenrs)
 
 -- Toggleterm
-api.nvim_set_keymap('n', '<leader>tt', ':ToggleTerm<CR>', {expr = false, noremap = true})
-api.nvim_set_keymap('t', '<leader>tt', [[<C-\><C-n>:ToggleTerm<CR>]], {expr = false, noremap = true})
-api.nvim_set_keymap('n', '<leader>ts', ':ToggleTermSendCurrentLine<CR>', {expr = false, noremap = true})
-api.nvim_set_keymap('v', '<leader>ts', ':ToggleTermSendVisualSelection<CR>', {expr = false, noremap = true})
+api.nvim_set_keymap('n', '<leader>tt', ':ToggleTerm<CR>', nenrs)
+api.nvim_set_keymap('t', '<leader>tt', [[<C-\><C-n>:ToggleTerm<CR>]], nenrs)
+api.nvim_set_keymap('n', '<leader>tT', ':ToggleTermToggleAll<CR>', nenrs)
+api.nvim_set_keymap('t', '<leader>tT', [[<C-\><C-n>:ToggleTermToggleAll<CR>]], nenrs)
+api.nvim_set_keymap('n', '<leader>ts', ':ToggleTermSendCurrentLine<CR>', nenrns)
+api.nvim_set_keymap('v', '<leader>ts', ':ToggleTermSendVisualSelection<CR>', nenrns)
 
 local Terminal  = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({ cmd = 'lazygit', hidden = true, direction = 'float', id = 999 })
+local lazygit = Terminal:new({
+  id = 999,
+  cmd = 'lazygit',
+  hidden = true,
+  direction = 'float',
+  close_on_exit = true,
+})
 function _lazygit_toggle()
   lazygit:toggle()
 end -- lazygit terminal
-vim.api.nvim_set_keymap('n', '<leader>g', '<cmd>lua _lazygit_toggle()<CR>', {noremap = true, silent = false})
+vim.api.nvim_set_keymap('n', '<leader>g', '<cmd>lua _lazygit_toggle()<CR>', nenrns)
 
 -- Telescope
-api.nvim_set_keymap('n', '<leader>fF', ':Telescope find_files<CR>', {expr = false, noremap = true})
-api.nvim_set_keymap('n', '<leader>ff', ':Telescope git_files<CR>', {expr = false, noremap = true})
-api.nvim_set_keymap('n', '<leader>fa', ':Telescope live_grep<CR>', {expr = false, noremap = true})
-api.nvim_set_keymap('n', '<leader>bb', ':Telescope buffers<CR>', {expr = false, noremap = true})
-api.nvim_set_keymap('n', '<leader>fb', ':Telescope current_buffer_fuzzy_find<CR>', {expr = false, noremap = true})
-api.nvim_set_keymap('n', '<leader>hf', ':Telescope help_tags<CR>', {expr = false, noremap = true})
+api.nvim_set_keymap('n', '<leader>fF', ':Telescope find_files<CR>', nenrs)
+api.nvim_set_keymap('n', '<leader>ff', ':Telescope git_files<CR>', nenrs)
+api.nvim_set_keymap('n', '<leader>fa', ':Telescope live_grep<CR>', nenrs)
+api.nvim_set_keymap('n', '<leader>bb', ':Telescope buffers<CR>', nenrs)
+api.nvim_set_keymap('n', '<leader>fb', ':Telescope current_buffer_fuzzy_find<CR>', nenrs)
+api.nvim_set_keymap('n', '<leader>hf', ':Telescope help_tags<CR>', nenrs)
 --------End mappings config--------
--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -83,14 +83,16 @@ return require('packer').startup(function()
     'akinsho/toggleterm.nvim',
     config = function()
       require('toggleterm').setup {
+        size = 15,
         hide_numbers = true,
         shade_terminals = true,
+        shading_factor = 2,
         start_in_insert = true,
         insert_mappings = true,
         terminal_mappings = true,
         persist_size = true,
         persist_mode = true,
-        direction = 'float',
+        direction = 'horizontal',
         close_on_exit = true,
       }
     end
