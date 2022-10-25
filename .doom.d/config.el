7a147c2aef8ef0b07a1a1986b9b2dd2780ec464f
Khoa Nguyen <khoadnguyen.se@gmail.com>
2022-10-25 13:57:36 +0700

    theme and font

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -22,7 +22,7 @@
 ;; accept. For example:
 ;;
 (setq doom-font (font-spec :family "JetBrains Mono" :size 13 :weight 'normal)
-      doom-variable-pitch-font (font-spec :family "JetBrains Mono" :size 13))
+      doom-variable-pitch-font (font-spec :family "JetBrains Mono" :size 12))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'tango)
+(setq doom-theme 'doom-zenburn)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
