c0626e3a96f9d4eb2e0de948a4cc8ae724cf3a68
Khoa Nguyen <khoan@instaclustr.com>
2023-01-30 22:00:08 +0700

    disable dim inactive windows option

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -202,7 +202,6 @@ return require('packer').startup(function()
       vim.g.gruvbox_material_foreground = 'original'
       vim.g.gruvbox_material_background = 'hard'
       vim.g.gruvbox_material_better_performance = 1
-      vim.g.gruvbox_material_dim_inactive_windows = 1
       vim.cmd([[ colorscheme gruvbox-material ]])
     end
   }
