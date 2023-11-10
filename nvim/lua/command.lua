91d12e4bdcbef27411a035383403e7a76ff0b45a
Khoa Nguyen <mail@khoanguyen.dev>
2023-11-10 10:49:56 +0700

    disable auto remove trailing spaces

--- nvim/lua/command.lua
+++ nvim/lua/command.lua
@@ -1,13 +1,13 @@
 --------Commands config--------
 -- Auto remove trailing spaces
-vim.cmd([[
-  augroup utils_user_config
-    autocmd!
-    " Auto remove trailing spaces
-    autocmd BufWritePre * %s/\s\+$//e
-    " Stop insert comment sign at new line automatically
-    autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
-  augroup end
-]])
+-- vim.cmd([[
+--   augroup utils_user_config
+--     autocmd!
+--     " Auto remove trailing spaces
+--     autocmd BufWritePre * %s/\s\+$//e
+--     " Stop insert comment sign at new line automatically
+--     autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
+--   augroup end
+-- ]])
 --------End commands config--------
 
