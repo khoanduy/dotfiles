43f338fcb711caa60e8fc6d5243462bbcf6a52cc
Khoa Nguyen <mail@khoanguyen.dev>
2024-01-31 13:17:37 +0700

    fix statusline

--- nvim/lua/vimc/settings.lua
+++ nvim/lua/vimc/settings.lua
@@ -60,17 +60,19 @@ vim.cmd([[
 -- Style statusline
 vim.cmd([[
   set laststatus=3
-  set statusline=%1*
-  set statusline+=%{StatuslineMode()}
-  set statusline+=\ 
-  set statusline+=%3*
+  set statusline=
+  set statusline+=%4*
+  set statusline+=%=
+  set statusline+=%{b:gitbranch}
+  set statusline+=%2*
   set statusline+=%f
   set statusline+=\ 
   set statusline+=%m
-  set statusline+=%=
-  set statusline+=%2*
-  set statusline+=%{b:gitbranch}
+  set statusline+=\ 
   set statusline+=%3*
+  set statusline+=%y
+  set statusline+=\ 
+  set statusline+=%4*
   set statusline+=%{strlen(&fenc)?&fenc:'none'}
   set statusline+=\|
   set statusline+=%l
@@ -78,11 +80,15 @@ vim.cmd([[
   set statusline+=%L
   set statusline+=\ 
   set statusline+=%2*
-  set statusline+=%y
+  set statusline+=\ 
+  set statusline+=%1*
+  set statusline+=%{StatuslineMode()}
+  set statusline+=\ 
 
   hi User1 ctermbg=NONE ctermfg=lightgreen guibg=NONE guifg=lightgreen
   hi User2 ctermbg=NONE ctermfg=lightcyan guibg=NONE guifg=lightcyan
-  hi User3 ctermbg=NONE ctermfg=grey guibg=NONE guifg=grey
+  hi User3 ctermbg=NONE ctermfg=lightyellow guibg=NONE guifg=lightyellow
+  hi User4 ctermbg=NONE ctermfg=grey guibg=NONE guifg=grey
 
   function! StatuslineMode()
     let l:mode=mode()
