6eeda57cc1fb6d3acd2364b3e9e2d96b809a214a
Khoa Nguyen <mail@khoanguyen.dev>
2024-03-28 14:36:32 +0700

    base16 default dark colorscheme

--- nvim/lua/vimc/plugins/init.lua
+++ nvim/lua/vimc/plugins/init.lua
@@ -92,15 +92,6 @@ lazy.setup({
   'mfussenegger/nvim-jdtls',
 
   -- UI
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('vimc/plugins/colorscheme')
-      vim.cmd([[ colorscheme tokyonight ]])
-    end
-  },
   'ibhagwan/fzf-lua',
   {
     'lukas-reineke/indent-blankline.nvim',
@@ -115,27 +106,16 @@ lazy.setup({
       })
     end
   },
-},
-{
-  ui = {
-    icons = {
-      cmd = '⌘',
-      config = '⚙',
-      event = '◔',
-      ft = '◊',
-      init = '◍',
-      keys = '⏼',
-      plugin = '▧',
-      runtime = '↺',
-      require = '⏾',
-      source = '◉',
-      start = '⏵',
-      task = '⌾',
-      lazy = '⏿ ',
-    },
-  },
 })
 
--- Load custom plugins
+-- Base16 default dark
+require('vimc/plugins/colorscheme').setup({
+    base00 = '#181818', base01 = '#282828', base02 = '#383838', base03 = '#585858',
+    base04 = '#b8b8b8', base05 = '#d8d8d8', base06 = '#e8e8e8', base07 = '#f8f8f8',
+    base08 = '#ab4642', base09 = '#dc9656', base0A = '#f7ca88', base0B = '#a1b56c',
+    base0C = '#86c1b9', base0D = '#7cafc2', base0E = '#ba8baf', base0F = '#a16946'
+})
+
+-- Load custom statusline
 require('vimc/plugins/statusline')
 -- End plugins' configuration --
--- nvim/lua/vimc/plugins/statusline.lua
+++ nvim/lua/vimc/plugins/statusline.lua
@@ -27,11 +27,13 @@ vim.cmd([[
   set statusline+=%1*
   set statusline+=%{StatuslineMode()}
   set statusline+=\ 
+
   hi User1 ctermbg=NONE ctermfg=lightgreen guibg=NONE guifg=lightgreen
   hi User2 ctermbg=NONE ctermfg=lightcyan guibg=NONE guifg=lightcyan
   hi User3 ctermbg=NONE ctermfg=lightyellow guibg=NONE guifg=lightyellow
   hi User4 ctermbg=NONE ctermfg=grey guibg=NONE guifg=grey
   hi User5 ctermbg=NONE ctermfg=lightred guibg=NONE guifg=lightred
+
   function! StatuslineMode()
     let l:mode=mode()
     if l:mode==#'n'
