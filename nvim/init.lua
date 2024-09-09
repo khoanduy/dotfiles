b3cc05058568c01cd5a1ddc2610b7b7cbabcfc8a
Khoa Nguyen <mail@khoanguyen.dev>
2024-09-09 18:14:15 +0700

    neovim basic config

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+------------------------
+-- Configuration file --
+------------------------
+
+-- Make sure to set `mapleader` first
+vim.g.mapleader = ' '
+
+-- Load configuration files --
+require('options')
+require('keymaps')
+
+------------------------
+-- Configuration file --
+------------------------
